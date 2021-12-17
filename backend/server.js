fd934ef809b415eb881536f61576908ecae228dc
Abhishek46-8 <79709311+Abhishek46-8@users.noreply.github.com>
2021-12-17 20:17:44 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const passportConfig = require("./lib/passportConfig");
+const cors = require("cors");
+const fs = require("fs");
+const MONGODB_URI="mongodb+srv://Abhi:test@cluster0.herbu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+// MongoDB
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((res) => console.log("Connected to DB"))
+  .catch((err) => console.log(err));
+
+// initialising directories
+if (!fs.existsSync("./public")) {
+  fs.mkdirSync("./public");
+}
+if (!fs.existsSync("./public/resume")) {
+  fs.mkdirSync("./public/resume");
+}
+if (!fs.existsSync("./public/profile")) {
+  fs.mkdirSync("./public/profile");
+}
+
+const app = express();
+var port= 5000 ;
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+// Setting up middlewares
+app.use(cors());
+app.use(express.json());
+app.use(passportConfig.initialize());
+
+// Routing
+app.use("/auth", require("./routes/authRoutes"));
+app.use("/api", require("./routes/apiRoutes"));
+app.use("/upload", require("./routes/uploadRoutes"));
+app.use("/host", require("./routes/downloadRoutes"));
+
+app.listen(process.env.PORT ||5000, () => {
+  console.log(`Server started on port ${port}!`);
+});
